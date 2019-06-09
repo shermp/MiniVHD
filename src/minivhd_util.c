7c82b74b1deee64a7ff452d9522ab1da4c8507a6
shermp <14854761+shermp@users.noreply.github.com>
2019-06-10 08:57:59 +1200

    Add timestamp functions

--- src/minivhd_util.c
+++ src/minivhd_util.c
@@ -5,7 +5,7 @@
 #ifdef HAVE_UUID_H
 #include <uuid/uuid.h>
 #endif
-#include "minivhd_internal.h"
+#include "minivhd_util.h"
 #include "minivhd.h"
 
 const char MVHD_CONECTIX_COOKIE[] = "conectix";
@@ -38,4 +38,22 @@ void mvhd_generate_uuid(uint8_t *uuid)
     uuid[8] &= 0x3F;
     uuid[8] |= 0x80; /* Variant 1 */
 #endif
+}
+
+/* Calculate the current timestamp. */
+uint32_t vhd_calc_timestamp(void)
+{
+        time_t start_time;
+        time_t curr_time;
+        double vhd_time;
+        start_time = MVHD_START_TS; /* 1 Jan 2000 00:00 */
+        curr_time = time(NULL);
+        vhd_time = difftime(curr_time, start_time);
+        return (uint32_t)vhd_time;
+}
+time_t vhd_get_created_time(MVHDMeta *vhdm)
+{
+        time_t vhd_time = (time_t)vhdm->footer.timestamp;
+        time_t vhd_time_unix = MVHD_START_TS + vhd_time;
+        return vhd_time_unix;
 }
--- src/minivhd_util.h
+++ src/minivhd_util.h
@@ -2,6 +2,7 @@
 #define MINIVHD_UTIL_H
 
 #include <stdint.h>
+#define MVHD_START_TS 946684800
 
 bool mvhd_is_conectix_str(const void* buffer);
 void mvhd_generate_uuid(uint8_t *uuid);
