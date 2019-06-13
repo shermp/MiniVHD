dd287341bc311a61c47ba75cf9384d7ab4452049
shermp <14854761+shermp@users.noreply.github.com>
2019-06-13 18:48:09 +1200

    Comment io file

--- src/minivhd_io.c
+++ src/minivhd_io.c
@@ -23,6 +23,16 @@ static void mvhd_write_bat_entry(MVHDMeta* vhdm, int blk);
 static void mvhd_create_block(MVHDMeta* vhdm, int blk);
 static void mvhd_write_curr_sect_bitmap(MVHDMeta* vhdm);
 
+/**
+ * \brief Check that we will not be overflowing buffers
+ * 
+ * \param [in] offset The offset from which we are beginning from
+ * \param [in] num_sectors The number of sectors which we desire to read/write
+ * \param [in] total_sectors The total number of sectors available
+ * \param [out] transfer_sect The number of sectors to actually write. 
+ * This may be lower than num_sectors if offset + num_sectors >= total_sectors
+ * \param [out] trunc_sectors The number of sectors truncated if transfer_sectors < num_sectors
+ */
 static inline void mvhd_check_sectors(int offset, int num_sectors, int total_sectors, int* transfer_sect, int* trunc_sect) {
     *transfer_sect = num_sectors;
     *trunc_sect = 0;
@@ -32,6 +42,15 @@ static inline void mvhd_check_sectors(int offset, int num_sectors, int total_sec
     }
 }
 
+/**
+ * \brief Write zero filled sectors to file.
+ * 
+ * Note, the caller should set the file position before calling this 
+ * function for correct operation.
+ * 
+ * \param [in] f File to write sectors to
+ * \param [in] sector_count The number of sectors to write
+ */
 static void mvhd_write_empty_sectors(FILE* f, int sector_count) {
     uint8_t zero_bytes[MVHD_SECTOR_SIZE] = {0};
     for (int i = 0; i < sector_count; i++) {
@@ -39,6 +58,15 @@ static void mvhd_write_empty_sectors(FILE* f, int sector_count) {
     }
 }
 
+/**
+ * \brief Read the sector bitmap for a block.
+ * 
+ * If the block is sparse, the sector bitmap in memory will be 
+ * zeroed. Otherwise, the sector bitmap is read from the VHD file.
+ * 
+ * \param [in] vhdm MiniVHD data structure
+ * \param [in] blk The block for which to read the sector bitmap from
+ */
 static void mvhd_read_sect_bitmap(MVHDMeta* vhdm, int blk) {
     if (vhdm->block_offset[blk] != MVHD_SPARSE_BLK) {
         fseeko64(vhdm->f, vhdm->block_offset[blk] * MVHD_SECTOR_SIZE, SEEK_SET);
@@ -49,6 +77,11 @@ static void mvhd_read_sect_bitmap(MVHDMeta* vhdm, int blk) {
     vhdm->bitmap.curr_block = blk;
 }
 
+/**
+ * \brief Write the current sector bitmap in memory to file
+ * 
+ * \param [in] vhdm MiniVHD data structure
+ */
 static void mvhd_write_curr_sect_bitmap(MVHDMeta* vhdm) {
     if (vhdm->bitmap.curr_block >= 0) {
         int64_t abs_offset = vhdm->block_offset[vhdm->bitmap.curr_block] * MVHD_SECTOR_SIZE;
@@ -57,6 +90,12 @@ static void mvhd_write_curr_sect_bitmap(MVHDMeta* vhdm) {
     }
 }
 
+/**
+ * \brief Write block offset from memory into file
+ * 
+ * \param [in] vhdm MiniVHD data structure
+ * \param [in] blk The block for which to write the offset for
+ */
 static void mvhd_write_bat_entry(MVHDMeta* vhdm, int blk) {
     uint64_t table_offset = vhdm->sparse.bat_offset + (blk * sizeof *vhdm->block_offset);
     uint32_t offset = cpu_to_be32(vhdm->block_offset[blk]);
@@ -64,6 +103,20 @@ static void mvhd_write_bat_entry(MVHDMeta* vhdm, int blk) {
     fwrite(&offset, sizeof offset, 1, vhdm->f);
 }
 
+/**
+ * \brief Create an empty block in a sparse or differencing VHD image
+ * 
+ * VHD images store data in blocks, which are typically 4096 sectors in size 
+ * (~2MB). These blocks may be stored on disk in any order. Blocks are created 
+ * on demand when required.
+ * 
+ * This function creates new, empty blocks, by replacing the footer at the end of the file 
+ * and then re-inserting the footer at the new file end. The BAT table entry for the
+ * new block is updated with the new offset.
+ * 
+ * \param [in] vhdm MiniVHD data structure
+ * \param [in] blk The block number to create
+ */
 static void mvhd_create_block(MVHDMeta* vhdm, int blk) {
     uint8_t footer[MVHD_FOOTER_SIZE];
     /* Seek to where the footer SHOULD be */
@@ -98,6 +151,21 @@ static void mvhd_create_block(MVHDMeta* vhdm, int blk) {
     mvhd_write_bat_entry(vhdm, blk);
 }
 
+/**
+ * \brief Read a fixed VHD image
+ * 
+ * Fixed VHD images are essentially raw image files with a footer tacked on 
+ * the end. They are therefore straightforward to write
+ * 
+ * \param [in] vhdm MiniVHD data structure
+ * \param [in] offset Sector offset to read from
+ * \param [in] num_sectors The desired number of sectors to read
+ * \param [out] out_buff An output buffer to store read sectors. Must be 
+ * large enough to hold num_sectors worth of sectors.
+ * 
+ * \retval 0 num_sectors were read from file
+ * \retval >0 < num_sectors were read from file
+ */
 int mvhd_fixed_read(MVHDMeta* vhdm, int offset, int num_sectors, void* out_buff) {
     int64_t addr;
     int transfer_sectors, truncated_sectors;
@@ -109,6 +177,24 @@ int mvhd_fixed_read(MVHDMeta* vhdm, int offset, int num_sectors, void* out_buff)
     return truncated_sectors;
 }
 
+/**
+ * \brief Read a sparse VHD image
+ * 
+ * Sparse, or dynamic images are VHD images that grow as data is written to them. 
+ * 
+ * This function implements the logic to read sectors from the file, taking into 
+ * account the fact that blocks may be stored on disk in any order, and that the 
+ * read could cross block boundaries.
+ * 
+ * \param [in] vhdm MiniVHD data structure
+ * \param [in] offset Sector offset to read from
+ * \param [in] num_sectors The desired number of sectors to read
+ * \param [out] out_buff An output buffer to store read sectors. Must be 
+ * large enough to hold num_sectors worth of sectors.
+ * 
+ * \retval 0 num_sectors were read from file
+ * \retval >0 < num_sectors were read from file
+ */
 int mvhd_sparse_read(MVHDMeta* vhdm, int offset, int num_sectors, void* out_buff) {
     int transfer_sectors, truncated_sectors;
     int total_sectors = vhdm->footer.geom.cyl * vhdm->footer.geom.heads * vhdm->footer.geom.spt;
@@ -141,6 +227,27 @@ int mvhd_sparse_read(MVHDMeta* vhdm, int offset, int num_sectors, void* out_buff
     return truncated_sectors;
 }
 
+/**
+ * \brief Read a differencing VHD image
+ * 
+ * Differencing images are a variant of a sparse image. They contain the grow-on-demand 
+ * properties of sparse images, but also reference a parent image. Data is read from the 
+ * child image only if it is newer than the data stored in the parent image.
+ * 
+ * This function implements the logic to read sectors from the child, or a parent image. 
+ * Differencing images may have a differencing image as a parent, creating a chain of images. 
+ * There is no theoretical chain length limit, although I do not consider long chains to be 
+ * advisable. Verifying the parent-child relationship is not very robust.
+ * 
+ * \param [in] vhdm MiniVHD data structure
+ * \param [in] offset Sector offset to read from
+ * \param [in] num_sectors The desired number of sectors to read
+ * \param [out] out_buff An output buffer to store read sectors. Must be 
+ * large enough to hold num_sectors worth of sectors.
+ * 
+ * \retval 0 num_sectors were read from file
+ * \retval >0 < num_sectors were read from file
+ */
 int mvhd_diff_read(MVHDMeta* vhdm, int offset, int num_sectors, void* out_buff) {
     int transfer_sectors, truncated_sectors;
     int total_sectors = vhdm->footer.geom.cyl * vhdm->footer.geom.heads * vhdm->footer.geom.spt;
@@ -173,6 +280,21 @@ int mvhd_diff_read(MVHDMeta* vhdm, int offset, int num_sectors, void* out_buff) 
     return truncated_sectors;
 }
 
+/**
+ * \brief Write to a fixed VHD image
+ * 
+ * Fixed VHD images are essentially raw image files with a footer tacked on 
+ * the end. They are therefore straightforward to write
+ * 
+ * \param [in] vhdm MiniVHD data structure
+ * \param [in] offset Sector offset to write to
+ * \param [in] num_sectors The desired number of sectors to write
+ * \param [in] in_buff A source buffer to write sectors from. Must be 
+ * large enough to hold num_sectors worth of sectors.
+ * 
+ * \retval 0 num_sectors were written to file
+ * \retval >0 < num_sectors were written to file
+ */
 int mvhd_fixed_write(MVHDMeta* vhdm, int offset, int num_sectors, void* in_buff) {
     int64_t addr;
     int transfer_sectors, truncated_sectors;
@@ -184,6 +306,29 @@ int mvhd_fixed_write(MVHDMeta* vhdm, int offset, int num_sectors, void* in_buff)
     return truncated_sectors;
 }
 
+/**
+ * \brief Write to a sparse or differencing VHD image
+ * 
+ * Sparse, or dynamic images are VHD images that grow as data is written to them. 
+ * 
+ * Differencing images are a variant of a sparse image. They contain the grow-on-demand 
+ * properties of sparse images, but also reference a parent image. Data is always written 
+ * to the child image. This makes writing to differencing images essentially identical to 
+ * writing to sparse images, hence they use the same function.
+ * 
+ * This function implements the logic to write sectors to the file, taking into 
+ * account the fact that blocks may be stored on disk in any order, and that the 
+ * write operation could cross block boundaries.
+ * 
+ * \param [in] vhdm MiniVHD data structure
+ * \param [in] offset Sector offset to write to
+ * \param [in] num_sectors The desired number of sectors to write
+ * \param [in] in_buff A source buffer to write sectors from. Must be 
+ * large enough to hold num_sectors worth of sectors.
+ * 
+ * \retval 0 num_sectors were written to file
+ * \retval >0 < num_sectors were written to file
+ */
 int mvhd_sparse_diff_write(MVHDMeta* vhdm, int offset, int num_sectors, void* in_buff) {
     int transfer_sectors, truncated_sectors;
     int total_sectors = vhdm->footer.geom.cyl * vhdm->footer.geom.heads * vhdm->footer.geom.spt;
@@ -225,6 +370,18 @@ int mvhd_sparse_diff_write(MVHDMeta* vhdm, int offset, int num_sectors, void* in
     return truncated_sectors;
 }
 
+/**
+ * \brief A no-op function to "write" to read-only VHD images
+ * 
+ * \param [in] vhdm MiniVHD data structure
+ * \param [in] offset Sector offset to write to
+ * \param [in] num_sectors The desired number of sectors to write
+ * \param [in] in_buff A source buffer to write sectors from. Must be 
+ * large enough to hold num_sectors worth of sectors.
+ * 
+ * \retval 0 num_sectors were written to file
+ * \retval >0 < num_sectors were written to file
+ */
 int mvhd_noop_write(MVHDMeta* vhdm, int offset, int num_sectors, void* in_buff) {
     return 0;
 }
