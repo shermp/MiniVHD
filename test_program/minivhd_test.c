257e6f313781882a5c4c07b11b294392f97bf641
shermp <14854761+shermp@users.noreply.github.com>
2019-11-15 20:36:21 +1300

    Expand test program with conversion

--- test_program/minivhd_test.c
+++ test_program/minivhd_test.c
@@ -1,23 +1,67 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "../src/minivhd.h"
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        printf("One argument, the full path to a VHD file is expected.\n");
+    if (argc != 6) {
+        char *help_text = 
+            "Incorrect num arguments. Expected args as follows:\n"
+            "minivhd_test RAW_SRC, VHD_FIXED, VHD_SPARSE, RAW_DEST_FIXED, RAW_DEST_SPARSE\n";
+        printf(help_text);
         return 1;
     }
-    char* path = argv[1];
+    const char *raw_src_path = argv[1];
+    const char *vhd_fixed_path = argv[2];
+    const char *vhd_sparse_path = argv[3];
+    const char *raw_dest_fixed_path = argv[4];
+    const char *raw_dest_sparse_path = argv[5];
     int err;
-    MVHDMeta* vhdm = mvhd_open(path, false, &err);
-    mvhd_close(vhdm);
-    /* Creation Tests */
-    MVHDGeom geom = mvhd_calculate_geometry(512 * 1024 * 1024);
-    vhdm = mvhd_create_sparse("C:/StandaloneProg/pcem/Images/minivhd/sparse.vhd", geom, &err);
+    /* Conversion tests. This just happens to test VHD creation, 
+     * and the read/write functionality all in one go :) */
+    time_t start, end;
+    printf("Converting raw imaged to fixed VHD\n");
+    start = time(0);
+    MVHDMeta *vhdm = mvhd_convert_to_vhd_fixed(raw_src_path, vhd_fixed_path, &err);
     if (vhdm == NULL) {
-        printf("Failed with exit code: %d", err);
-        return 1;
+        printf("%s\n", mvhd_strerr(err));
+        return EXIT_FAILURE;
     }
+    end = time(0);
+    printf("Raw image converted to fixed VHD in %f seconds\n", difftime(end, start));
     mvhd_close(vhdm);
-    return 0;
+
+    printf("Converting raw imaged to sparse VHD\n");
+    start = time(0);
+    vhdm = mvhd_convert_to_vhd_sparse(raw_src_path, vhd_sparse_path, &err);
+    if (vhdm == NULL) {
+        printf("%s\n", mvhd_strerr(err));
+        return EXIT_FAILURE;
+    }
+    end = time(0);
+    printf("Raw image converted to fixed VHD in %f seconds\n", difftime(end, start));
+    mvhd_close(vhdm);
+
+    printf("Converting fixed VHD to raw image\n");
+    start = time(0);
+    FILE *raw = mvhd_convert_to_raw(vhd_fixed_path, raw_dest_fixed_path, &err);
+    if (raw == NULL) {
+        printf("%s\n", mvhd_strerr(err));
+        return EXIT_FAILURE;
+    }
+    fclose(raw);
+    end = time(0);
+    printf("Fixed VHD converted to raw image in %f seconds\n", difftime(end, start));
+
+    printf("Converting sparse VHD to raw image\n");
+    start = time(0);
+    raw = mvhd_convert_to_raw(vhd_sparse_path, raw_dest_sparse_path, &err);
+    if (raw == NULL) {
+        printf("%s\n", mvhd_strerr(err));
+        return EXIT_FAILURE;
+    }
+    fclose(raw);
+    end = time(0);
+    printf("Sparse VHD converted to raw image in %f seconds\n", difftime(end, start));
+    return EXIT_SUCCESS;
 }
